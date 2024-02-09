59c3139775a2dd9b222d6e7abf7898ee66a7767b
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 15:23:43 -0300

    Project creation!

---
+++ src/main/java/com/luv2code/springbootaopdemo/SpringbootAopdemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbootaopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAopdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luv2code/springbootaopdemo/SpringbootAopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbootaopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
