cd52bd5cea67db28913f2ddfc34dd0e1ebe0b60f
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-02-09 15:41:03 -0300

    Calling addAccount method from AccountDAO on program start.

--- src/main/java/com/luv2code/springbootaopdemo/SpringbootAopdemoApplication.java
+++ src/main/java/com/luv2code/springbootaopdemo/SpringbootAopdemoApplication.java
@@ -1,13 +1,33 @@
 package com.luv2code.springbootaopdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.luv2code.springbootaopdemo.dao.AccountDAO;
 
 @SpringBootApplication
 public class SpringbootAopdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
+
+		return runner -> {
+
+			demoTheBeforeAdvice(theAccountDAO);
+		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+		
+		// call the business metho
+		theAccountDAO.addAccount();
+		
 	}
 
 }
